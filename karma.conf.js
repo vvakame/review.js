13deecf5bd0d59721e27d541c07826195ffb4332
vvakame <vvakame+dev@gmail.com>
2014-07-28 11:53:44 +0900

    travisが落ちてたのを修正

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function (config) {
 		// list of files / patterns to load in the browser
 		files: [
 			'test/suite/libs/es5-shim.js',
+			'test/suite/libs/promise.js',
 			'test/suite/libs/i18next.js',
 			'test/suite/libs/power-assert.js',
 			'test/test.js'
