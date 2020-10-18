e3282294707a4263ea71a90defa1bffea8b5f043
yfakariya <yfakariya@gmail.com>
2020-10-19 00:21:18 +0900

    Add test for cross chapter @<hd>{...}

--- test/suite/controller/controllerSpec.ts
+++ test/suite/controller/controllerSpec.ts
@@ -1,8 +1,11 @@
 import * as assert from "power-assert";
 
-import { start } from "../../../lib/index";
+import { Builder, start } from "../../../lib/index";
 
 import { TextBuilder } from "../../../lib/builder/textBuilder";
+import { HtmlBuilder } from "../../../lib/builder/htmlBuilder";
+
+import { Book } from "../../../lib/model/compilerModel";
 
 describe("ReVIEW.Controllerの", () => {
     "use strict";
@@ -94,4 +97,67 @@ describe("ReVIEW.Controllerの", () => {
             assert(book.contents[1].builderProcesses[0].result === "■H1■第2章　章2\n\n");
         });
     });
+
+    function testCrossChapterHeadingReference(builder: Builder, assertion: (book: Book) => void) {
+        let files: any = {
+            "ch01.re": "={ch01} ちゃぷたーだよ\n\n@<hd>{ch02|sec}を見るのだ",
+            "ch02.re": "={ch02} チャプター！\n\n=={sec} せくしょん\n\nここに本文がある"
+        };
+        let result: any = {
+        };
+        return start(review => {
+            review.initConfig({
+                read: (path: string) => {
+                    return Promise.resolve(files[path]);
+                },
+                write: (path: string, content: any) => {
+                    result[path] = content;
+                    return Promise.resolve<null>(null);
+                },
+
+                listener: {
+                    onCompileSuccess: () => {
+                    },
+                    onCompileFailed: () => {
+                    }
+                },
+
+                builders: [builder],
+
+                book: {
+                    contents: [
+                        { file: "ch01.re" },
+                        { file: "ch02.re" }
+                    ]
+                }
+            });
+        }).then(book => {
+            assert(book.contents.length === 2);
+            book.contents.forEach(chunk => {
+                assert(!!chunk.tree.ast);
+            });
+
+            assertion(book);
+        });
+    }
+
+    it("章を越えた@<hd>{...}が正しく動くこと（Text）", () =>
+        testCrossChapterHeadingReference(
+            new TextBuilder(),
+            book => {
+                assert(book.contents[0].builderProcesses.length > 0);
+                assert(book.contents[0].builderProcesses[0].result === "■H1■第1章　ちゃぷたーだよ\n\n「2.1 せくしょん」を見るのだ\n");
+            }
+        )
+    );
+
+    it("章を越えた@<hd>{...}が正しく動くこと（HTML）", () =>
+        testCrossChapterHeadingReference(
+            new HtmlBuilder(),
+            book => {
+                assert(book.contents[0].builderProcesses.length > 0);
+                assert(book.contents[0].builderProcesses[0].result.includes("<p>「2.1 せくしょん」を見るのだ</p>"));
+            }
+        )
+    );
 });
