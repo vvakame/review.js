11955d69793d7e7a82593fc98026c3399e183b53
yfakariya <yfakariya@gmail.com>
2020-10-25 01:52:56 +0900

    Re-enable inline title test cases

--- test/suite/rubyReVIEWDiffSpec.ts
+++ test/suite/rubyReVIEWDiffSpec.ts
@@ -70,7 +70,6 @@ describe("Ruby版ReVIEWとの出力差確認", () => {
             "lead", // ブロック構文内でのParagraphの扱いがおかしいのを直していない
             "preface", // めんどくさいので
             "preproc",  // めんどくさいので
-            "inline_title", // Ruby版の処理がおかしい https://github.com/kmuto/review/issues/624
             "inline_chapref", // Ruby版の処理がおかしい https://github.com/kmuto/review/issues/624
             "inline_chap", // Ruby版の処理がおかしい https://github.com/kmuto/review/issues/624
             "inline_comment", // Ruby版の処理がおかしい https://github.com/kmuto/review/pull/625
